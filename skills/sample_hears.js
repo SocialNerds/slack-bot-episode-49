c53068649a024f1f3a218160fb2a410e32eab9d9
Thanos Korakas <thanoskorakas@gmail.com>
2018-04-21 18:49:01 +0300

    Initial commit

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -15,6 +15,34 @@ module.exports = function(controller) {
         bot.reply(message, "Hi there, you're on workspace: " + message.team)
     });
 
+    controller.hears(['form'], 'direct_message,direct_mention', function(bot, message) {
+        bot.reply(message, {
+            attachments:[
+                {
+                    title: 'Do you want to interact with my buttons?',
+                    callback_id: '123',
+                    attachment_type: 'default',
+                    actions: [
+                        {
+                            "name":"yes",
+                            "text": "Yes",
+                            "value": "yes",
+                            "type": "button",
+                        },
+                        {
+                            "name":"no",
+                            "text": "No",
+                            "value": "no",
+                            "type": "button",
+                        }
+                    ]
+                }
+            ]
+        })
+    });
 
-
+    controller.on('interactive_message_callback', function(bot, message) {
+        bot.replyInteractive(message, 'interactive message :O'
+        );
+    });
 };
